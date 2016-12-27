15d8009b72975f90708b437622682f2f87b61585
joshragem <barney@whoop.com>
2016-12-26 21:49:13 -0500

    able to save and read out a rust str

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,16 @@ extern crate libc;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::mem;
+    use std::slice;
+    use std::str;
     use std::ffi::CString;
     use std::ptr::null_mut;
-    use std::sync::Arc;
-    // use libc::perror;
+    use std::os::raw::c_void;
 
     #[test]
     fn it_works() {
@@ -39,6 +41,36 @@ mod tests {
             let mut cptr: *mut MDB_cursor = &mut crs;
             let res_crs = mdb_cursor_open(tptr, dbi, &mut cptr as *mut _);
             assert_eq!(0, res_crs);
+            let mut crs2: MDB_cursor = mem::zeroed();
+            let mut cptr2: *mut MDB_cursor = &mut crs2;
+            let res_crs2 = mdb_cursor_open(tptr, dbi, &mut cptr2 as *mut _);
+            assert_eq!(0, res_crs2);
+            let keystr = CString::new("hello").unwrap();
+            let keylen: usize = keystr.to_bytes().len();
+            let keyptr: *mut c_void = keystr.as_ptr() as *mut c_void;
+            let mut keyval: MDB_val = MDB_val { mv_size: keylen, mv_data: keyptr };
+            let valstr = "garbage";
+            let vallen: usize = mem::size_of_val(valstr);
+            let valptr: *mut c_void = valstr.as_ptr() as *mut c_void;
+            let mut valval: MDB_val = MDB_val { mv_size: vallen, mv_data: valptr };
+            let res_put = mdb_cursor_put(cptr, &mut keyval as *mut _, &mut valval as *mut _, MDB_NOOVERWRITE);
+            assert_eq!(0, res_put);
+            mdb_cursor_close(cptr);
+            mdb_cursor_close(cptr2);
+            mdb_txn_commit(tptr);
+            let res_txn2 = mdb_txn_begin(eptr, null_mut(), 0, &mut tptr as *mut _);
+            assert_eq!(0, res_txn2);
+            let mut valnew: MDB_val = MDB_val { mv_size: 0, mv_data: null_mut() };
+            mdb_cursor_open(tptr, dbi, &mut cptr as *mut _);
+            let res_get = mdb_cursor_get(cptr, &mut keyval as *mut _, &mut valnew as *mut _, MDB_cursor_op::MDB_FIRST);
+            assert_eq!(0, res_get);
+            assert_eq!(valval.mv_size, valnew.mv_size);
+            //let junk = &mut *(valval.mv_data as *mut u8);
+            //let junk2 = &mut *(valnew.mv_data as *mut u8);
+            //assert_eq!(junk, junk2);
+            let junk: &[u8] = slice::from_raw_parts(valval.mv_data as *const u8, valval.mv_size);
+            let junkstr = str::from_utf8(junk).unwrap();
+            assert_eq!(valstr, junkstr);
         }
     }
 }
